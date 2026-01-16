5afcefd7aa0fbcdcc4ecce897372d1ac7cba0441
oohtee2000 <oluwatosinissa2000@gmail.com>
2026-01-16 01:37:38 +0100

    fix build error

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,21 +1,10 @@
+import { Suspense } from "react";
 import ProductsList from "@/components/products/ProductsList";
 
-// const dummyProducts = [
-//   {
-//     id: "1",
-//     name: "Stylish Sneakers",
-//     price: "$120",
-//     offer: "Limited Time Offer",
-//     colors: [
-//       { name: "Black", value: "#000" },   
-//       { name: "Red", value: "#f00" },
-//     ],
-//     images: [
-//       "https://cdn.pixabay.com/photo/2023/08/25/07/37/shoes-8212405_1280.jpg",
-//       "https://cdn.pixabay.com/photo/2016/03/27/18/23/sneakers-1283091_1280.jpg",
-//     ],
-//   },
-// ];
 export default function Page() {
-  return <ProductsList />;
+  return (
+    <Suspense fallback={<div className="p-6">Loading products...</div>}>
+      <ProductsList />
+    </Suspense>
+  );
 }
